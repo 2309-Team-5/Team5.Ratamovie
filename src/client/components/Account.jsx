3efc739a7f2a8aecd979431d861bc9e3764867f0
Derek <derekmlafontaine@gmail.com>
2023-12-11 14:04:05 -0600

    account page

--- src/client/components/Account.jsx
+++ src/client/components/Account.jsx
@@ -1,184 +1,147 @@
 import { useState, useEffect } from 'react'
-import Axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+const API = "http://localhost:3000/api"
 // eslint-disable-next-line react/prop-types
-function Account({ token }) {
-  // const [userData, setUserData] = useState({});
-  // const [reviewedMovies, setReviewedMovies] =useState([]);
-  // let API = "http://localhost:3000/api"
-  const [name, SetName] = useState('')
-  const [lastname, SetLastName] = useState('')
-  const [email, SetEmail] = useState('')
-  const [reservedMovies, setReservedMovies] = useState([])
-  const [ user, setOwner ] = useState({})
+
+function Account({ token, setToken }) {
   
+  
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+ 
+  const [userData, setUserData] = useState({});
+  const [reviewedMovies, setReviewedMovies] =useState([]);
 
-  useEffect(() =>{
-    async function fetchUser() {
-      // await fetchAccount();
-      // await fetchReviewedMovies();
-      // setLoading(false);
-      try{
-        const API = "http://localhost:3000/api"
-        const response = await Axios.get(`${API}/users/me`, {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-          },
-        })
-        // fetchUser()
-      const userData = response.data
 
-      SetName(userData.firstname)
-      SetLastName(userData.lastname)
-      SetEmail(userData.email)
-
-      const reserveResponse = await Axios.get(`${API}/reservations`, {
+  // const { id } = useParams();
+  
+  useEffect(() => {
+    fetchAccount();
+    fetchReviewedMovies();
+    fetchAccountInfo();
+    handleEditProfile();
+  }, [token]);
+  
+  async function fetchAccountInfo(userId) {
+    try {
+      const response = await fetch (`${API}/users/${userId}`, {
+        method: "GET",
         headers: {
-          'Authorization': `Bearer ${token}`,
-        }
-      })
-      if(Array.isArray(reserveResponse.data.reservation)) {
-        setReservedMovies(reserveResponse.data.reservation)
-      } else {
-        console.error('Wrong data', reserveResponse.data)
-      }
-    } catch (err) {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+      });
+
+      const result = await response.json();
+      setUserData(result);
+
+    } catch(err) {
       console.error(err)
     }
   }
-    if (token) {
-      fetchUser()
-    }
-  }, [token])
-  const handleDelete = async (movieId) => {
-    try{
-      const API = "http://localhost:3000/api"
-      await Axios.delete(`${API}/reservations/${movieId}`, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      })
-      setReservedMovies((beforeReservedMovies) => 
-      beforeReservedMovies.filter((movie) => movie.id !== movieId)
-      )
-    } catch (error) {
-      console.error('error delete', error)
+  
+  async function fetchAccount(){
+      if (token){
+        try{ 
+          const response = await fetch (`${API}/users/me`,
+          {
+          headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        });
+        const result = await response.json();
+        setUserData(result);
+      }catch(error){
+        console.error(error.message)
+      }
+    }else{
+      console.log("Sorry, you are not logged in!")
+      
     }
   }
-  return (
-    <div className="account-container">
-      <h1>Account Information</h1>
-      <h2>{name} {lastname} </h2>
-      <h4>{email}</h4>
-        <div>
-          <h2>Reserved  </h2>
-          <ul>
-         
-          </ul>
-        </div>
-    </div>
-  )
-}
-    
-  
-                        
+  async function fetchReviewedMovies(){
+    if (token){
+      try{
+        const response = await fetch(`${API}/users/reviews`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        const result = await response.json();
+        setReviewedMovies(result.reviews);
+      } catch(error) {
+        console.error(error.message);
+      }
+    }
+  }
 
-  
-//   async function fetchAccount(){
-//       if (token){
-//         try{ 
-//           const response = await fetch (`${API}/users/me`,
-//           {
-//           headers:{
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`
-//           },
-//         });
-//         const result = await response.json();
-//         setUserData(result);
-//       }catch(error){
-//         console.error(error.message)
-//       }
-//     }else{
-//       console.log("Sorry, you are not logged in!")
+  async function handleEditProfile() {
+    try{
+
+
       
-//     }
-//   }
-//   async function fetchReviewedMovies(){
-//     if (token){
-//       try{
-//         const response = await fetch(`${API}/users/reviews`, {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`,
-//           },
-//         });
-//         const result = await response.json();
-//         setReviewedMovies(result.reviews);
-//       } catch(error) {
-//         console.error(error.message);
-//       }
-//     }
-//   }
-
-//   async function handleEditProfile() {
-//     try{
-//       const response = await fetch(`${API}/users/me`, {
-//         method: 'PATCH',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${token}`,
-//         },
-//         body: JSON.stringify({
-//           newUsername,
-//           newPassword,
-//         }),
-//       });
-//       if (response.ok){
-//         fetchAccount();
-//         setNewUsername('');
-//         setNewPassword('');
-//         setEditProfileMode(false);
-//       } else {
-//         console.error('Failed to update profile:', response.statusText);
-//       }
-//     } catch (error){
-//       console.error('Error updating your profile:', error.message);
-//     }
-//   };
-//     return (
-//       <div>
+      const response = await fetch(`${API}/users/me`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          newUsername: newUsername,
+          newPassword: newPassword,
+        }),
+      });
+      if (response.ok){
+        // fetchAccountInfo();
+        // fetchAccount();
+        setNewUsername('');
+        setNewPassword('');
+        setEditProfileMode(false);
+        setToken();
         
-//         { token ? (
-//             <div>
-//           <h2>My Account</h2>
-//               <p>Name: {userData.firstname} {userData.lastname}</p>
-//               <p>Username: {userData.email}</p>
-//          <h3>Reviewed Movies:</h3>
-//          { reviewedMovies.length > 0 ? (
+      
+      } else {
+        console.error('Failed to update profile:', response.statusText);
+      }
+    } catch (error){
+      console.error('Error updating your profile:', error.message);
+    }
+  };
+ 
+    return (
+      <div>
+        { token ? (
+            <div>
+          <h2>My Account</h2>
+              <p>Name: {userData.firstname} {userData.lastname}</p>
+              <p>Username: {userData.email}</p>
+         <h3>Reviewed Movies:</h3>
+         { reviewedMovies.length > 0 ? (
 
-//          <ul>    
-//           {reviewedMovies.map((movie) => {
-//             return (
-//                 <li key={movie.id}>
-//                   {movie.title}
-//                   {movie.year}
-//                   {/* {movie.userid.review} */}
-//                   {/*CHANGE THIS TO BE WHEN U CLICK ON THE REVIEW YOU GET THE SINGLE PAGE RENDERED <button onClick={() => handleReturn(book.id)}>Return</button> */}
-//                 </li>
-//             )
-//           } )}
-       
-//          </ul>
-         
-//          ) : (
-//           <h2> You havent reserved any movies yet!!</h2>
-//          )}
-//     </div>
-//          ) : (
-//           <h3> logged in</h3>
-//          )}
-//      </div>
-// );
-// }
+         <ul>    
+          {reviewedMovies.map((movie) => {
+            return (
+                <li key={movie.id}>
+                  { movie.title }
+                  { movie.year }
+                  { movie.userid.review }
+                  {/* { <button onClick={() => handleReturn(movie.id)}>Return</button> } */}
+                </li>
+            )
+          } )}
+         </ul>
+         ) : (
+          <h2> You havent reserved any movies yet!!</h2>
+         )}
+    </div>
+         ) : (
+          <h3>Sorry! You are not logged in! Please login or register to see this page!</h3>
+         )}
+     </div>
+);
+}
 
 export default Account
